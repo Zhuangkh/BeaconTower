4d32d0e6568c0cb76597714fdd77c692e107ad22
Fat Person <58603432@qq.com>
2021-02-06 14:45:53 +0800

    add trace datetime str
    add trace datetime str

--- src/UI/src/pages/display/nodetrace/path/traceItemTable/index.tsx
+++ src/UI/src/pages/display/nodetrace/path/traceItemTable/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Table } from "antd";
 import React, { FC, useState, useEffect } from "react"
 import { ResponseCode } from "../../../../../api/common";
-import { GetTraceIDListByNodeAndPathAlias } from "../../../../../api/resource/nodes";
+import { GetTraceBeginTime, GetTraceIDListByNodeAndPathAlias } from "../../../../../api/resource/nodes";
 import "./index.less"
 
 interface indexProps {
@@ -24,13 +24,33 @@ const index: FC<indexProps> = (props) => {
         width: "500px",
         dataIndex: "traceID"
     }, {
+        title: "发生时间",
+        width: "200px",
+        dataIndex: "dateTime"
+    },{
         title: "操作",
         render: (item: any) => {
-            return <Button type="primary" shape="round" onClick={() => { 
+            return <Button type="primary" shape="round" onClick={() => {
                 props.onSelectTraceItem(item.traceID);
             }}>查看trace详情</Button>
         }
     }];
+
+    const fetchTraceDateTimeInfo = async (data: Array<any>) => {
+        console.log(data);
+        const taskList = [];
+        for (let index = 0; index < data.length; index++) {
+            console.log(data);
+            const element = data[index];
+            let loader=async () => {
+                let res = GetTraceBeginTime(props.nodeAlias, props.pathAlias as string, element.traceID);
+                element.dateTime= (await res).data;
+            };
+            taskList.push(loader());
+        }
+        await Promise.all(taskList);
+        setData([...data]);
+    }
 
     const fetch = async (currentIndex: number) => {
         if (props.pathAlias == null) return;
@@ -41,10 +61,13 @@ const index: FC<indexProps> = (props) => {
             resData.forEach((element: string) => {
                 currentPageData.push({
                     key: element,
-                    traceID: element
+                    traceID: element,
+                    dateTime:"获取中.."
                 });
             });
+            fetchTraceDateTimeInfo(currentPageData);
             setData([...currentPageData]);
+
             setTotalData(res.total);
         }
     }
